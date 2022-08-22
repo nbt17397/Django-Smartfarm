81eddd6001b00a90ae772e88025760c4e6928b16
Mason <tu.nguyenba@haophuong.com>
2022-08-22 13:09:59 +0700

    login oauth

--- smartfarm/settings.py
+++ smartfarm/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': '10',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
         'knox.auth.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
