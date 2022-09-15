4d911c45b55eeb9ddc33b5f230f1a333676aa7dd
Mason <tu.nguyenba@haophuong.com>
2022-09-15 13:54:40 +0700

    hash password when update user

--- hatchery/serializers.py
+++ hatchery/serializers.py
@@ -27,7 +27,11 @@ class UserSerializer(ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        instance.set_password(validated_data['password'])
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
         instance.save()
         return instance
 
