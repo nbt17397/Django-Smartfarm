e39b630ab024fa6c7396d2407b374f115c2c78e4
Mason <tu.nguyenba@haophuong.com>
2022-11-17 16:47:29 +0700

    .

--- smartfarm/settings.py
+++ smartfarm/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 from rest_framework.settings import api_settings
@@ -199,6 +200,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = '%s/hatchery/static' % BASE_DIR
 
 # Default primary key field type
