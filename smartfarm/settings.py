e6853c7d619b80b93c5d728ae640673034262394
Mason <tu.nguyenba@haophuong.com>
2022-11-18 10:05:07 +0700

    .

--- smartfarm/settings.py
+++ smartfarm/settings.py
@@ -67,7 +67,6 @@ INSTALLED_APPS = [
     'oauth2_provider',
     'knox',
     "corsheaders",
-    "django_admin_logs",
 ]
 
 MIDDLEWARE = [
--- smartfarm/urls.py
+++ smartfarm/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('hatchery.urls')),
     path('admin/', admin.site.urls),
-    path('logs/', include('log_viewer.urls')),
 ]
 
 
